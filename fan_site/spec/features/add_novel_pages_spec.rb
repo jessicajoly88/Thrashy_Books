7140edd85b43653a3fdd8c6428ad55085c5e6c6e
Guest User <jessicajoly88@gmail.com>
2015-10-14 11:16:03 -0700

    Integration specs for add new novel process pass.

---
+++ fan_site/spec/features/add_novel_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "the add a novel process" do 
+  it "adds a new novel" do
+    visit novels_path
+    click_on "Add a novel"
+    fill_in 'Author', :with => "Danielle Steele"
+    fill_in 'Description', :with => "Hot and Steamy!"
+    fill_in 'Title', :with => "Betrayal"
+    click_on 'Create Novel'
+    expect(page).to have_content "Betrayal"
+  end
+
+  it "gives error when no title or description or author is entered" do
+    visit new_novel_path
+    click_on 'Create Novel'
+    expect(page).to have_content 'errors'
+  end
+end
