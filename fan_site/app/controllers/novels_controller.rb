8924908fff4ef946a9fc780226cc4bba9349e2ce
Guest User <jessicajoly88@gmail.com>
2015-10-14 11:06:00 -0700

    Add partials for error message and new novel form.

--- fan_site/app/controllers/novels_controller.rb
+++ fan_site/app/controllers/novels_controller.rb
@@ -6,4 +6,24 @@ class NovelsController < ApplicationController
   def show
     @novel = Novel.find(params[:id])
   end
+
+  def new
+    @novel = Novel.new
+  end
+
+  def create
+    @novel = Novel.new(novel_params)
+    @novel.rating = 0
+    if @novel.save
+      redirect_to novels_path
+    else
+      render :new
+    end
+  end
+
+private
+  def novel_params
+    params.require(:novel).permit(:author, :title, :description)
+  end
+
 end
