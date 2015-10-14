9d87211fa38e0813bfb6e236a6bd170369197f77
Guest User <jessicajoly88@gmail.com>
2015-10-14 10:49:17 -0700

    Add controller function and views page for Novel index and show.

--- fan_site/app/controllers/novels_controller.rb
+++ fan_site/app/controllers/novels_controller.rb
@@ -2,4 +2,8 @@ class NovelsController < ApplicationController
   def index
     @novels = Novel.all
   end
+
+  def show
+    @novel = Novel.find(params[:id])
+  end
 end
--- fan_site/app/models/novel.rb
+++ fan_site/app/models/novel.rb
@@ -2,5 +2,4 @@ class Novel < ActiveRecord::Base
   validates :author, :presence => true
   validates :title, :presence => true
   validates :description, :presence => true
-  validates :rating, :presence => true
 end
